af0ff60db69f83b5bac2ddf09204e2c9fdce9fed
Morgend <andrey.pokidov@gmail.com>
2019-02-11 14:08:49 +0700

    +3D Position

---
+++ Geometry/Position3.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+/*
+ * Author: Andrey Pokidov
+ * Date: 5 Feb 2019
+ * 3D position (3D coordinate system)
+ */
+
+namespace MathKit.Geometry
+{
+    public class Position3
+    {
+        public Vector3 Point;
+
+        private Rotation rotation;
+
+        public Position3()
+        {
+            this.Point = new Vector3();
+            this.rotation = new Rotation();
+        }
+
+        public Position3(Position3 position)
+        {
+            this.Point = position.Point;
+            this.rotation = new Rotation(position.rotation);
+        }
+
+        public Position3(Vector3 point, EulerAngles angles)
+        {
+            this.Point = point;
+            this.rotation = new Rotation(angles);
+        }
+
+        public Position3(Vector3 point, Angle heading, Angle elevation, Angle bank)
+        {
+            this.Point = point;
+            this.rotation = new Rotation(heading, elevation, bank);
+        }
+
+        public Position3(Vector3 point, double heading, double elevation, double bank)
+        {
+            this.Point = point;
+            this.rotation = new Rotation(heading, elevation, bank);
+        }
+
+        public Position3(Position3 first, Position3 second)
+        {
+            this.rotation = new Rotation();
+            this.setCombinationOf(first, second);
+        }
+
+        public Rotation Rotation
+        {
+            get
+            {
+                return this.rotation;
+            }
+
+            set
+            {
+                this.rotation.copyOf(value);
+            }
+        }
+
+        public Position3 combineWith(Position3 position)
+        {
+            return new Position3(this, position);
+        }
+
+        public void setCombinationOf(Position3 first, Position3 second)
+        {
+            if (first == null || second == null)
+            {
+                throw new NullReferenceException("An instance of Position was expected but NULL was got");
+            }
+
+            this.Point = first.Point + first.rotation.turn(second.Point);
+            this.rotation.setCombinationOf(first.rotation, second.rotation);
+        }
+
+        public void invert()
+        {
+            this.Point = this.rotation.turnBackward(-this.Point);
+            this.rotation.invert();
+        }
+
+        public Position3 getInverted()
+        {
+            Position3 result = new Position3(this);
+            result.invert();
+            return result;
+        }
+
+        public Vector3 toParentPositioning(Vector3 vector)
+        {
+            return this.rotation.turn(vector) + this.Point;
+        }
+
+        public Vector3 fromParentPositioning(Vector3 vector)
+        {
+            return this.rotation.turnBackward(vector - this.Point);
+        }
+
+        public Vector3 changePositioningTo(Position3 position, Vector3 vector)
+        {
+            if (position == null)
+            {
+                throw new ArgumentNullException("An instance of Position3 was expected but NULL was got");
+            }
+
+            if (position == this)
+            {
+                return vector;
+            }
+
+            return position.rotation.turnBackward(this.rotation.turn(vector) + this.Point - position.Point);
+        }
+
+        public Vector3 changePositioningFrom(Position3 position, Vector3 vector)
+        {
+            if (position == null)
+            {
+                throw new ArgumentNullException("An instance of Position3 was expected but NULL was got");
+            }
+
+            if (position == this)
+            {
+                return vector;
+            }
+
+            return this.rotation.turnBackward(position.rotation.turn(vector) + position.Point - this.Point);
+        }
+    }
+}
