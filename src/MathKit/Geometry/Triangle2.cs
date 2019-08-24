850e0b07297ea3f284fe251cb130b67140aecb65
Andrey Pokidov <andrey.pokidov@gmail.com>
2019-08-25 00:26:23 +0700

    +Triangles

---
+++ src/MathKit/Geometry/Triangle2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MathKit.Geometry
+{
+    public struct Triangle2
+    {
+        public Vector2 A;
+        public Vector2 B;
+        public Vector2 C;
+
+        public Triangle2(Vector2 A, Vector2 B, Vector2 C)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+
+        public Triangle2(Triangle2 anotherTriangle)
+        {
+            A = anotherTriangle.A;
+            B = anotherTriangle.B;
+            C = anotherTriangle.C;
+        }
+
+        public double GetSquare()
+        {
+            return 0.5 * Math.Abs((B.x - A.x) * (C.y - A.y) - (B.y - A.y) * (C.x - A.x));
+        }
+
+        public Vector2 GetMedianCentre()
+        {
+            return (A + B + C) / 3.0;
+        }
+    }
+}
---
+++ src/MathKit/Geometry/Triangle3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MathKit.Geometry
+{
+    public struct Triangle3
+    {
+        public Vector3 A;
+        public Vector3 B;
+        public Vector3 C;
+
+        public Triangle3(Vector3 A, Vector3 B, Vector3 C)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+
+        public Triangle3(Triangle3 anotherTriangle)
+        {
+            A = anotherTriangle.A;
+            B = anotherTriangle.B;
+            C = anotherTriangle.C;
+        }
+
+        public double GetSquare()
+        {
+            return 0.5 * (B - A).Vector(C - A).Module();
+        }
+
+        public Vector3 GetMedianCentre()
+        {
+            return (A + B + C) / 3.0;
+        }
+
+        public Vector3 GetNormal()
+        {
+            Vector3 n = (B - A).Vector(C - A);
+            n.Normalize();
+            return n;
+        }
+    }
+}
